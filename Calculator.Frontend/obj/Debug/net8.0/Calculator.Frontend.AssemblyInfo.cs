0fd9f22e03d010e2fc3c3b1cdc4b4c073022875a
Desertrat31794 <124222768+Desertrat31794@users.noreply.github.com>
2024-04-22 20:28:27 -0400

    Got the damn thing working

--- Calculator.Frontend/obj/Debug/net8.0/Calculator.Frontend.AssemblyInfo.cs
+++ Calculator.Frontend/obj/Debug/net8.0/Calculator.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f764c07055ae64eda3180ae1b540eeda7edfd97e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5914cd819b51330c40dba6396df444648e8996")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
